a94704678e3f8d6526dbb64b1a70f86c97e694e2
sumahh <tkanyingi23@gmail.com>
2020-10-10 22:51:30 +0300

    broken snake pls help

--- Parts/Snake/game.js
+++ Parts/Snake/game.js
@@ -1,14 +1,11 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const scale = 20;
-
 const rows = canvas.height / scale;
 const columns = canvas.width / scale;
-
 const size = 580;
 const bsize = 29;
-
-var score;
+var score = 0;
 
 console.log("There are " + rows + " rows");
 console.log("There are " + columns + " columns");
@@ -18,8 +15,24 @@ let snake = [];
 snake[0] ={
     x:Math.floor((bsize/2)) * scale, 
     y:Math.floor((bsize/2)) * scale
-}
+};
 
+
+let dir; document.addEventListener("keydown", direction); 
+
+//Seting the direction being pressed by arrow keys
+function direction(event)
+{
+    if(event.keyCode == 37)
+        dir = "LEFT";
+    if(event.keyCode == 38)
+        dir = "UP";    
+    if(event.keyCode == 39)
+        dir = "RIGHT";    
+    if(event.keyCode == 40)
+        dir = "DOWN";    
+
+}
 //choosing the placement of the food
 
 var fx;
@@ -35,20 +48,7 @@ food = {
 }
 
 
-let dir; document.addEventListener("keydown", direction); 
-//Seting the direction being pressed by arrow keys
-function direction(event)
-{
-    if(event.keyCode == 37)
-        dir = "LEFT";
-    if(event.keyCode == 38)
-        dir = "UP";    
-    if(event.keyCode == 39)
-        dir = "RIGHT";    
-    if(event.keyCode == 40)
-        dir = "DOWN";    
 
-}
 
 //drawing function
 function draw(){
@@ -67,43 +67,61 @@ function draw(){
     let snakeY = snake[0].y;
     
     if(dir == "LEFT")
-        snake[0].x -= scale;
+        snakeX -= scale;
 
     if(dir == "RIGHT")
-        snake[0].x += scale;
+        snakeX += scale;
 
     if(dir == "UP")
-        snake[0].y -= scale;
+        snakeY -= scale;
 
     if(dir == "DOWN")
-        snake[0].y += scale;
+        snakeY += scale;
  
-    
-    //Eating the food
-    if(snake[0].x == food.x && snake[0].y == food.y){
-        score +=1;
-        console.log(score)
+     //Eating the food
+    if(snakeX == food.x && snakeY == food.y){
+        
         fx = Math.floor(Math.random() * 28) + 1;
         fy = Math.floor(Math.random() * 28) + 1;
+        food.x = (bsize-fx)*scale;
+        food.y = (bsize-fy)*scale;
+        score +=1;
+        console.log(score);
+   
+    }
+    else {
+        snake.pop();
+    }   
 
-     }
+        let newHead = {
+        x : snakeX,
+        y : snakeY
+        };
+    
+       //Collision detection
+    function collision(head, array){
+        for(let i = 0; i<array; i++){
+          if(head.x == array[i].x && head.y == array[i].y ){
+            return true;
+          };  
+        };
+     };
+        return false;
+
+        if(snakeX < scale || snakeY < scale ||
+           snakeX > ((bsize - 1) * scale) || snakeY > ((bsize - 1) * scale) ||
+           collision(newHead, snake))
+        {
+            clearInterval(game);
+        };
+
+    snake.unshift(newHead);
+
     //Making the food
     ctx.fillStyle = "red";
     ctx.fillRect( food.x, food.y, scale, scale);
-
-  
-
+}
     
-
-                   
-
-
-    }
-
-
- 
-
-
 
 let game = setInterval(draw, 100);
 
