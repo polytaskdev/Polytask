4b0cd979353640c8f0c3b61884bb6a3f9486b93f
advancetech01 <baokou66@gmail.com>
2020-10-17 17:52:22 -0400

    added timer/clock

--- window.js
+++ window.js
@@ -35,27 +35,30 @@ var addNote = () => {
     note = create(`<div class="part-container note" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>notes</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Notes/index.html"></iframe></div>`)
     container.appendChild(note);
 }
+
+
+
 //deletes window
 var closeWindow = (element) => {
-	element.parentNode.remove();
+    element.parentNode.remove();
 }
 
 //makes window go to top when clicked
-var goTop = (element) =>{
+var goTop = (element) => {
     let elements = document.getElementsByClassName('part-container');
     let zI = 0;
     for (let i = 0; i < elements.length; i++) {
         let index = parseInt(elements[i].style.zIndex);
-        if(!index){index = 0}
-        if(index > parseInt(element.style.zIndex)&& element != elements[i]){
-            elements[i].style.zIndex = parseInt(elements[i].style.zIndex)-1
+        if (!index) { index = 0 }
+        if (index > parseInt(element.style.zIndex) && element != elements[i]) {
+            elements[i].style.zIndex = parseInt(elements[i].style.zIndex) - 1
         }
-        if(index>zI){
+        if (index > zI) {
             zI = index;
         }
     }
     element.style.zIndex = zI;
-    zI=0;
+    zI = 0;
 }
 
 // returns the highest z-index so that all of the windows have differnt z-indexes
@@ -64,19 +67,19 @@ var returnTop = () => {
     let zIndex = 0;
     for (let i = 0; i < elements.length; i++) {
         let index = parseInt(elements[i].style.zIndex);
-        if(!index || index < 1){index = 0;elements[i].style.zIndex = 0}
+        if (!index || index < 1) { index = 0; elements[i].style.zIndex = 0 }
         console.log(index)
-        if(index > zIndex){zIndex = index}
+        if (index > zIndex) { zIndex = index }
 
     }
 
-    return(zIndex+1)
+    return (zIndex + 1)
 }
 
 
 
 //saving notes in localStorage so they don't disappear when reloaded
-function saveWindow(posX,posY,zI,width,height,type){
+function saveWindow(posX, posY, zI, width, height, type) {
     this.posX = posX;
     this.posY = posY;
     this.zI = zI;
@@ -86,42 +89,42 @@ function saveWindow(posX,posY,zI,width,height,type){
 }
 
 //makes json file and saves the windows
-function saveWindows(){
+function saveWindows() {
     let savedata = [];
     let savedWindows = document.getElementsByClassName('part-container');
-    for(let i = 0; i<savedWindows.length;i++){
+    for (let i = 0; i < savedWindows.length; i++) {
         let win = savedWindows[i];
         savedata[i] = new saveWindow(win.style.left, win.style.top, win.style.zIndex, win.style.width, win.style.height, win.classList[1]);
 
     }
-    localStorage.setItem('savedWindows',JSON.stringify(savedata));
+    localStorage.setItem('savedWindows', JSON.stringify(savedata));
 
 }
 
 //saves dark/light mode
-function saveTheme(){
+function saveTheme() {
     switch (appearance) {
         case 'auto':
-            localStorage.setItem('theme','auto');
+            localStorage.setItem('theme', 'auto');
             break;
         default:
         case 'dark':
-            localStorage.setItem('theme','dark');
+            localStorage.setItem('theme', 'dark');
             break;
         case 'light':
-            localStorage.setItem('theme','light');
+            localStorage.setItem('theme', 'light');
             break;
     }
 }
 
-window.addEventListener('beforeUnload',() => {saveWindows();saveTheme()})
-window.addEventListener('unload',() => {saveWindows();saveTheme()})
+window.addEventListener('beforeUnload', () => { saveWindows(); saveTheme() })
+window.addEventListener('unload', () => { saveWindows(); saveTheme() })
 
 //loads in the saved windows
-function loadWindows(){
+function loadWindows() {
     let savedata = JSON.parse(localStorage.getItem('savedWindows'));
     container = document.getElementById('main-container');
-    for(let i = 0; i<savedata.length; i++){
+    for (let i = 0; i < savedata.length; i++) {
         switch (savedata[i].type) {
             case 'calculator':
                 calculator = create(`<div class="part-container calculator" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>calculator</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Calculator/index.html"></iframe></div>`)
@@ -143,7 +146,7 @@ function loadWindows(){
 }
 
 //loads the theme
-function loadTheme(){
+function loadTheme() {
     let savedAppearance = localStorage.getItem('theme');
     switch (savedAppearance) {
         case 'auto':
@@ -156,4 +159,4 @@ function loadTheme(){
             darkmode()
     }
 }
-window.addEventListener('load',() => {loadWindows();loadTheme()});
+window.addEventListener('load', () => { loadWindows(); loadTheme() });
