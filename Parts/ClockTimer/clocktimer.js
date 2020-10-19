068a1db868961d620429538f0b2b2723b8103e13
advancetech01 <baokou66@gmail.com>
2020-10-19 08:49:24 -0400

    add and changed some variable

--- Parts/ClockTimer/clocktimer.js
+++ Parts/ClockTimer/clocktimer.js
@@ -3,13 +3,10 @@ function showTaps(event, tapName) {
     let tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tabContent.length; i++) {
         tabContent[i].style.display = "none";
-
     }
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" opened", "");
     }
     document.getElementById(tapName).style.display = "block";
-    event.currentTarget.className += " active";
-
-
+    event.currentTarget.className += " opened";
 }
