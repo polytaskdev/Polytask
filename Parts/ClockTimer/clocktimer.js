2bb57226045cdb7ef508561964c383fbfb45bed6
advancetech01 <baokou66@gmail.com>
2020-10-18 21:20:14 -0400

    updated the taps in index.html in Time/Clock

---
+++ Parts/ClockTimer/clocktimer.js
@@ -0,0 +1,15 @@
+function showTaps(event, tapName) {
+    let tabContent = document.getElementsByClassName("tabContent");
+    let tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+
+    }
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tapName).style.display = "block";
+    event.currentTarget.className += " active";
+
+
+}
