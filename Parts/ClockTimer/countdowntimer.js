c96a6c73a29ff94ba8e5460a34d0e6e7ba7391fe
TonyZ <tony.j.h.zhang@gmail.com>
2020-10-27 18:15:48 -0400

    made timer accurate by using Date.now() instead of set Interval

--- Parts/ClockTimer/countdowntimer.js
+++ Parts/ClockTimer/countdowntimer.js
@@ -1,49 +1,51 @@
-var start = document.getElementById('start-btn');
-var finished = document.getElementById('finished-btn');
+var start = document.getElementById("start-btn");
+var finished = document.getElementById("finished-btn");
 var hour = document.getElementById("hour-cdt");
 var minute = document.getElementById("minute-cdt");
 var sec = document.getElementById("sec-cdt");
 var startTimer = null;
-
+var endTime;
 function timer() {
-    hour.value = hour.value || 0
-    minute.value = minute.value || 0
-    sec.value = sec.value || 0
-
-    hour.value = parseInt(hour.value)
-    minute.value = parseInt(minute.value)
-    sec.value = parseInt(sec.value)
-    if (hour.value == 0 && minute.value == 0 && sec.value == 0) {
+    let currentTime = Date.now();
+    currentTime = Math.floor(currentTime / 1000);
+    let timeLeft = endTime - currentTime;
+    if (timeLeft <= 0) {
+        hour.value = "";
+        minute.value = "";
+        sec.value = "";
         stopInterval();
-    } else if (sec.value != 0) {
-        sec.value--;
-    } else if (minute.value != 0 && sec.value == 0) {
-        sec.value = 59;
-        minute.value--;
-    } else if (hour.value != 0 && minute.value == 0) {
-        minute.value = 60;
-        hour.value--;
+    } else {
+        hour.value = Math.floor(timeLeft / 3600);
+        minute.value = Math.floor((timeLeft % 3600) / 60);
+        sec.value = timeLeft % 60;
     }
-
-    return;
 }
 
-start.addEventListener('click', function () {
+start.addEventListener("click", function () {
     function startInterval() {
+        hour.value = hour.value || 0;
+        minute.value = minute.value || 0;
+        sec.value = sec.value || 0;
+
+        endTime = Date.now();
+        endTime = Math.floor(endTime / 1000);
+        endTime += parseInt(hour.value) * 3600;
+        endTime += parseInt(minute.value) * 60;
+        endTime += parseInt(sec.value);
         startTimer = setInterval(function () {
             timer();
         }, 1000);
     }
     startInterval();
-})
+});
 
 function stopInterval() {
     clearInterval(startTimer);
 }
 
-finished.addEventListener('click', function () {
-    hour.value = 0;
-    minute.value = 0;
-    sec.value = 0;
+finished.addEventListener("click", function () {
+    hour.value = "";
+    minute.value = "";
+    sec.value = "";
     stopInterval();
-})
+});
