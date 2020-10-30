1e1cd7b3f23dfc7ddabe02d91b942e8b58da4c47
advancetech01 <baokou66@gmail.com>
2020-10-29 20:02:36 -0400

    It is a long sentence about what I did because I want to be clear about what I did not some vague commit, what I did is I add some html of the stopWatch and added three function in StartWatch, PauseWatch, and Reset Watch. They are probably kinda messy lollololo.

---
+++ Parts/ClockTimer/stopwatch.js
@@ -0,0 +1,55 @@
+var startTime;
+var newTime;
+var difference;
+var interval;
+var savedTime;
+var paused = false;
+var running = false;
+
+
+
+function startWatch() {
+    if (!running) {
+        startTime = new Date().getTime();
+        interval = setInterval(function () {
+            newTime = new Date().getTime();
+            if (savedTime) {
+                difference = (newTime - startTime) + savedTime;
+            } else {
+                difference = newTime - startTime;
+            }
+            var mins = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+            var secs = Math.floor((difference % (1000 * 60)) / 1000);
+            var ms = Number(String(Math.floor((difference % (1000 * 60)) / 10)).slice(-2))
+            mins = (mins < 10) ? "0" + mins : mins;
+            secs = (secs < 10) ? "0" + secs : secs;
+            ms = (ms < 10) ? (ms < 10) ? "0" + ms : "0" + ms : ms;
+            document.getElementById("show-time").innerHTML = mins + ':' + secs + ':' + ms;
+        }, 1);
+        paused = false;
+        running = true;
+        document.getElementById("start-pause-swc").innerHTML = "Pause";
+    } else {
+        pauseWatch();
+    }
+}
+
+function pauseWatch() {
+    clearInterval(interval);
+    running = false;
+    paused = true;
+    savedTime = difference;
+    document.getElementById("start-pause-swc").innerHTML = "Resume";
+}
+
+function resetWatch() {
+    clearInterval(interval);
+    savedTime = 0;
+    difference = 0;
+    paused = false;
+    running = false;
+    document.getElementById("show-time").innerHTML = "00:00:00"
+    document.getElementById("start-pause-swc").innerHTML = "Start"
+}
+
+
