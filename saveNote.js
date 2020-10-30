4a3d2ccbd716843088a9a6dad2660a3b187cb2d2
TonyZ <tony.j.h.zhang@gmail.com>
2020-10-30 14:58:12 -0400

    made notes saving work(sortof)

---
+++ saveNote.js
@@ -0,0 +1,36 @@
+saveNotes = () => {
+    notes = document.getElementsByClassName('note');
+    saveNoteData = [];
+    for (note of notes) {
+        iframe = note.getElementsByTagName('iframe')[0];
+        header = iframe.contentWindow.document.getElementById('header').innerHTML;
+        content = iframe.contentWindow.document.getElementById('content').innerHTML;
+        noteinfo = new saveNote(header, content);
+        saveNoteData.push(noteinfo);
+    }
+    saveNoteData = localStorage.setItem('saveNotes', JSON.stringify(saveNoteData));
+};
+
+function saveNote(header, content) {
+    this.header = header;
+    this.content = content;
+}
+
+window.addEventListener('unload', saveNotes);
+window.addEventListener('beforeUnload', saveNotes);
+
+loadNote = () => {
+    notes = document.getElementsByClassName('note');
+    saveNoteData = JSON.parse(localStorage.getItem('saveNotes'));
+    for (let i = 0; i < notes.length; i++) {
+        let note = notes[i];
+        let iframe = note.getElementsByTagName('iframe');
+        console.log(iframe, i);
+        iframe[0].addEventListener('load', () => {
+            iframe[0].contentWindow.document.getElementById('header').innerHTML = saveNoteData[i].header;
+            iframe[0].contentWindow.document.getElementById('content').innerHTML = saveNoteData[i].content;
+        });
+    }
+};
+
+window.addEventListener('load', loadNote);
--- window.js
+++ window.js
@@ -10,45 +10,52 @@ function create(htmlStr) {
     return frag;
 }
 
-
 //creates a new calculator window
 var addCalculator = () => {
     container = document.getElementById('main-container');
-    calculator = create(`<div class="part-container calculator" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Calculator</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Calculator/index.html"></iframe></div>`);
+    calculator = create(
+        `<div class="part-container calculator" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Calculator</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Calculator/index.html"></iframe></div>`
+    );
     container.appendChild(calculator);
-}
+};
 
 //creates a new snake windows
 var addSnake = () => {
     container = document.getElementById('main-container');
-    snake = create(`<div class="part-container snake" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Snake</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Snake/index.html"></iframe></div>`);
+    snake = create(
+        `<div class="part-container snake" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Snake</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Snake/index.html"></iframe></div>`
+    );
     container.appendChild(snake);
-}
+};
 
 var addDictionary = () => {
     container = document.getElementById('main-container');
-    dictionary = create(`<div class="part-container dictionary" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Dictionary</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Dictionary/index.html"></iframe></div>`);
+    dictionary = create(
+        `<div class="part-container dictionary" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Dictionary</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Dictionary/index.html"></iframe></div>`
+    );
     container.appendChild(dictionary);
-}
+};
 
 var addNote = () => {
     container = document.getElementById('main-container');
-    note = create(`<div class="part-container note" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Notes</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Notes/index.html"></iframe></div>`);
+    note = create(
+        `<div class="part-container note" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Notes</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Notes/index.html"></iframe></div>`
+    );
     container.appendChild(note);
-}
+};
 
 var addTimer = () => {
     container = document.getElementById('main-container');
-    timer = create(`<div class="part-container timer" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Timer/Clock</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/ClockTimer/index.html"></iframe></div>`);
+    timer = create(
+        `<div class="part-container timer" onmousedown = "goTop(this)" style="z-index:${returnTop()}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Timer/Clock</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/ClockTimer/index.html"></iframe></div>`
+    );
     container.appendChild(timer);
-}
-
-
+};
 
 //deletes window
 var closeWindow = (element) => {
     element.parentNode.remove();
-}
+};
 
 //makes window go to top when clicked
 var goTop = (element) => {
@@ -59,7 +66,9 @@ var goTop = (element) => {
     //so the number will not increase
     for (let i = 0; i < elements.length; i++) {
         let index = parseInt(elements[i].style.zIndex);
-        if (!index) { index = 0 }
+        if (!index) {
+            index = 0;
+        }
         if (index > parseInt(element.style.zIndex) && element != elements[i]) {
             elements[i].style.zIndex = parseInt(elements[i].style.zIndex) - 1;
         }
@@ -70,7 +79,7 @@ var goTop = (element) => {
     //set the z_index of the element to be the hightest
     element.style.zIndex = zI;
     zI = 0;
-}
+};
 
 // returns the highest z-index so that all of the windows have differnt z-indexes
 // used when new windows are added and makes it so that the windows all have different z_indexes
@@ -79,15 +88,17 @@ var returnTop = () => {
     let zIndex = 0;
     for (let i = 0; i < elements.length; i++) {
         let index = parseInt(elements[i].style.zIndex);
-        if (!index || index < 1) { index = 0; elements[i].style.zIndex = 0 }
-        if (index > zIndex) { zIndex = index }
-
+        if (!index || index < 1) {
+            index = 0;
+            elements[i].style.zIndex = 0;
+        }
+        if (index > zIndex) {
+            zIndex = index;
+        }
     }
 
-    return (zIndex + 1);
-}
-
-
+    return zIndex + 1;
+};
 
 //saving notes in localStorage so they don't disappear when reloaded
 function saveWindow(posX, posY, zI, width, height, type) {
@@ -105,11 +116,16 @@ function saveWindows() {
     let savedWindows = document.getElementsByClassName('part-container');
     for (let i = 0; i < savedWindows.length; i++) {
         let win = savedWindows[i];
-        savedata[i] = new saveWindow(win.style.left, win.style.top, win.style.zIndex, win.style.width, win.style.height, win.classList[1]);
-
+        savedata[i] = new saveWindow(
+            win.style.left,
+            win.style.top,
+            win.style.zIndex,
+            win.style.width,
+            win.style.height,
+            win.classList[1]
+        );
     }
     localStorage.setItem('savedWindows', JSON.stringify(savedata));
-
 }
 
 //saves dark/light mode in localStorage
@@ -128,8 +144,14 @@ function saveTheme() {
     }
 }
 //runs the saveWindows and saveTheme functions when page is reloaded
-window.addEventListener('beforeUnload', () => { saveWindows(); saveTheme() })
-window.addEventListener('unload', () => { saveWindows(); saveTheme() })
+window.addEventListener('beforeUnload', () => {
+    saveWindows();
+    saveTheme();
+});
+window.addEventListener('unload', () => {
+    saveWindows();
+    saveTheme();
+});
 
 //loads in the saved windows
 function loadWindows() {
@@ -138,23 +160,48 @@ function loadWindows() {
     for (let i = 0; i < savedata.length; i++) {
         switch (savedata[i].type) {
             case 'calculator':
-                calculator = create(`<div class="part-container calculator" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Calculator</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Calculator/index.html"></iframe></div>`);
+                calculator = create(
+                    `<div class="part-container calculator" onmousedown = "goTop(this)" style="z-index:${savedata[i]
+                        .zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i]
+                        .width};height:${savedata[i]
+                        .height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Calculator</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Calculator/index.html"></iframe></div>`
+                );
                 container.appendChild(calculator);
                 break;
             case 'dictionary':
-                dictionary = create(`<div class="part-container dictionary" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Dictionary</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Dictionary/index.html"></iframe></div>`);
+                dictionary = create(
+                    `<div class="part-container dictionary" onmousedown = "goTop(this)" style="z-index:${savedata[i]
+                        .zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i]
+                        .width};height:${savedata[i]
+                        .height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Dictionary</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Dictionary/index.html"></iframe></div>`
+                );
                 container.appendChild(dictionary);
                 break;
             case 'snake':
-                snake = create(`<div class="part-container snake" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Snake</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Snake/index.html"></iframe></div>`);
+                snake = create(
+                    `<div class="part-container snake" onmousedown = "goTop(this)" style="z-index:${savedata[i]
+                        .zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i]
+                        .width};height:${savedata[i]
+                        .height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Snake</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Snake/index.html"></iframe></div>`
+                );
                 container.appendChild(snake);
                 break;
             case 'note':
-                note = create(`<div class="part-container note" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Notes</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Notes/index.html"></iframe></div>`);
+                note = create(
+                    `<div class="part-container note" onmousedown = "goTop(this)" style="z-index:${savedata[i]
+                        .zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i]
+                        .width};height:${savedata[i]
+                        .height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Notes</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/Notes/index.html"></iframe></div>`
+                );
                 container.appendChild(note);
                 break;
             case 'timer':
-                timer = create(`<div class="part-container timer" onmousedown = "goTop(this)" style="z-index:${savedata[i].zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i].width};height:${savedata[i].height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Timer/Clock</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/ClockTimer/index.html"></iframe></div>`);
+                timer = create(
+                    `<div class="part-container timer" onmousedown = "goTop(this)" style="z-index:${savedata[i]
+                        .zI};left:${savedata[i].posX};top:${savedata[i].posY};width:${savedata[i]
+                        .width};height:${savedata[i]
+                        .height}"><div class="drag-bar" onmousedown="dragStart(this)"><p>Timer/Clock</p></div><button onclick="closeWindow(this)">×</button><iframe src="Parts/ClockTimer/index.html"></iframe></div>`
+                );
                 container.appendChild(timer);
         }
     }
@@ -176,4 +223,7 @@ function loadTheme() {
 }
 
 //loads windows when the website loads
-window.addEventListener('load', () => { loadWindows(); loadTheme() });
+window.addEventListener('load', () => {
+    loadWindows();
+    loadTheme();
+});
